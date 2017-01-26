566202fbfa52929cb58640cfce48d2b3025c0ff8
Academia <Academia>
2017-01-25 21:52:22 -0800

    reorganize entities

---
+++ src/main/java/com/stk/ecom/java/academy/domain/Cart.java
@@ -0,0 +1,108 @@
+package com.stk.ecom.java.academy.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cart")
+public class Cart implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "cart_id", nullable = false)
+	private int carId;
+	
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customerId;
+	
+	@Column(name = "amount", nullable = false)
+	private double amount;
+	
+	@Embedded
+	private Audit audit;
+
+	public int getCarId() {
+		return carId;
+	}
+
+	public void setCarId(int carId) {
+		this.carId = carId;
+	}
+
+	public Customer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Customer customerId) {
+		this.customerId = customerId;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Audit getAudit() {
+		return audit;
+	}
+
+	public void setAudit(Audit audit) {
+		this.audit = audit;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(amount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((audit == null) ? 0 : audit.hashCode());
+		result = prime * result + carId;
+		result = prime * result + ((customerId == null) ? 0 : customerId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cart other = (Cart) obj;
+		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount))
+			return false;
+		if (audit == null) {
+			if (other.audit != null)
+				return false;
+		} else if (!audit.equals(other.audit))
+			return false;
+		if (carId != other.carId)
+			return false;
+		if (customerId == null) {
+			if (other.customerId != null)
+				return false;
+		} else if (!customerId.equals(other.customerId))
+			return false;
+		return true;
+	}
+
+	
+}
--- src/main/java/com/stk/ecom/java/academy/domain/Order.java
+++ src/main/java/com/stk/ecom/java/academy/domain/Order.java
@@ -3,8 +3,12 @@ package com.stk.ecom.java.academy.domain;
 import java.io.Serializable;
 
 import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -13,27 +17,90 @@ public class Order implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	@EmbeddedId
-	private OrderKey orderId;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "order_id", nullable = false)
+	private int orderId;
 	
-	@Column(name="quantity", nullable = false)
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product productId;
+	
+	@ManyToOne
+	@JoinColumn(name = "cart_id")
+	private Cart cartId;
+	
+	@Column(name = "quantity", nullable = false)
 	private int quantity;
-	
-	@Column(name="amount", nullable = false)
-	private double amount;
-	
-	
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public Product getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Product productId) {
+		this.productId = productId;
+	}
+
+	public Cart getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(Cart cartId) {
+		this.cartId = cartId;
+	}
+
 	public int getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-	public double getAmount() {
-		return amount;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cartId == null) ? 0 : cartId.hashCode());
+		result = prime * result + orderId;
+		result = prime * result + ((productId == null) ? 0 : productId.hashCode());
+		result = prime * result + quantity;
+		return result;
 	}
-	public void setAmount(double amount) {
-		this.amount = amount;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (cartId == null) {
+			if (other.cartId != null)
+				return false;
+		} else if (!cartId.equals(other.cartId))
+			return false;
+		if (orderId != other.orderId)
+			return false;
+		if (productId == null) {
+			if (other.productId != null)
+				return false;
+		} else if (!productId.equals(other.productId))
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		return true;
 	}
+
 	
 }
